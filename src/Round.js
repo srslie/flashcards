078ad632c99aeb8e05da3d93c91c715c27172ad5
srslie <aliceruppert@gmail.com>
2020-12-01 23:05:57 -0700

    Start Round class properties and methods

---
+++ src/Round.js
@@ -0,0 +1,31 @@
+const Turn = require('../src/Turn');
+
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.incorrectGuesses = [];
+    this.turns = 0;
+  }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns]
+  }
+
+  takeTurn(guess) {
+    let currentTurn = new Turn('guess', this.deck.cards[this.turns])
+    if (!currentTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(guess)
+    }
+    this.turns++
+  }
+
+  calculatePercentCorrect() {
+    return this.incorrectGuesses ? (Math.ceil(this.incorrectGuesses.length / this.deck.length) * 100) : 0
+  }
+
+  endRound() {
+    return `**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
+}
+
+module.exports = Round;
