dfec32e34034f7a7113036060e76b17506ac5555
srslie <aliceruppert@gmail.com>
2020-12-02 20:54:16 -0700

    Get exit or restart prompt to show, exit out of node

--- src/util.js
+++ src/util.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const Game = require('../src/Game');
+
 
 const genList = (round) => {
   let card = round.returnCurrentCard();
@@ -21,43 +23,46 @@ const getRound = (round) => {
   return Promise.resolve(round);
 }
 
-const confirmUpdate = (id, round) => {
-  const feedback = round.takeTurn(id);
+const confirmUpdate = (guess, round) => {
+  const feedback = round.takeTurn(guess);
   return {
     name: 'feedback',
-    message: `Your answer of ${id} is ${feedback}`
+    message: `Your answer of ${guess} is ${feedback}`
   }
 }
 
-// const exitGamePrompt = () => {
-//   return {
-//     name: 'response',
-//     message: 'Exit or Restart?',
-//     choices: [
-//       {key: 1, value: 'Exit'},
-//       {key: 2, value: 'Restart'}
-//     ]
-//   }
-// }
-//
-// const exitOrRestart = (response) => {
-//   if (getResponse ===  1) {
-//     control C?
-//   } else {
-//     let game = new Game
-//     game.start()
-//   }
-// }
+//Working on trying to get the game to prompt for a exit or restart after the Round ends
+const exitGamePrompt = () => {
+  return {
+    type: 'rawlist',
+    name: 'response',
+    message: 'Exit or Restart?',
+    choices: [
+      {key: 1, value: 'Exit'},
+      {key: 2, value: 'Restart'}
+    ]
+  }
+}
+
+const exitOrRestart = (response) => {
+  if (response ===  'Exit') {
+    process.exit();
+  } else {
+    let game = new Game()
+    game.start()
+  }
+}
 
 async function main(round) {
 
   const currentRound = await getRound(round);
   const getAnswer = await inquirer.prompt(genList(currentRound));
   const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round));
-  // const getResponse = await inquirer.prompt(exitGamePrompt())
 
     if(!round.returnCurrentCard()) {
       round.endRound();
+      const getResponse = await inquirer.prompt(exitGamePrompt())
+      exitOrRestart(getResponse.response)
     } else {
       main(round);
     }
